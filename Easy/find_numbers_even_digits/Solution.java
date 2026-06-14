16dd4322c5fdb09953864ea02542d09cae02d075
pastrananoelangelo-cmd <pastrananoelangelo@gmail.com>
2026-06-15 00:19:11 +0800

    Two Sum

---
+++ Easy/find_numbers_even_digits/Solution.java
@@ -0,0 +1,24 @@
+package find_numbers_even_digits;
+
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0, curr, count;
+
+        for (int i = 0; i < nums.length; i++) {
+            curr = nums[i];
+            count = 0;
+
+            while (curr > 0) {
+                curr /= 10;
+                count++;
+            }
+
+            if (count % 2 == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ Easy/two_sum/Solution.java
@@ -0,0 +1,22 @@
+package two_sum;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        int difference;
+
+        for (int i = 0; i < nums.length; i++) {
+            difference = target - nums[i];
+
+            if (map.containsKey(difference)) {
+                ans[0] = map.get(difference);
+                ans[1] = i;
+                return ans;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return ans;
+    }
+}

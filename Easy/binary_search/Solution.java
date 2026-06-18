1d929900fce575c007a7dc47173743be74ef208d
pastrananoelangelo-cmd <pastrananoelangelo@gmail.com>
2026-06-19 00:45:53 +0800

    Binary Search.

---
+++ Easy/binary_search/Solution.java
@@ -0,0 +1,22 @@
+package Easy.binary_search;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = (right + left) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}

7c54b91d49ffe5e975803b2c847ef36969a611f5
pastrananoelangelo-cmd <pastrananoelangelo@gmail.com>
2026-06-20 15:40:10 +0800

    Done with Phase 6

---
+++ Easy/search_insert_position/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid = 0;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        if (nums[left] < target) {
+            mid = left + 1;
+        } else {
+            mid = left;
+        }
+
+        return mid;
+    }
+}
---
+++ Medium/kth_largest_element_in_array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (heap.size() < k) {
+                heap.offer(nums[i]);
+            } else {
+                if (heap.peek() < nums[i]) {
+                    heap.poll();
+                    heap.offer(nums[i]);
+                }
+            }
+        }
+
+        return heap.peek();
+    }
+}
